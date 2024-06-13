916f3b9dde362b65161a8b2b40ddf68ed37b8891
WordlessMeteor <97432897+WordlessMeteor@users.noreply.github.com>
2024-06-13 22:42:51 +0800

    更新了文件忽略规则【Updated GitIgnore Rule】
    现在，文件名超过171个字符的文件不会上传到GitHub。
    Now, files whose names exceed 171 characters won't be uploaded to GitHub.

--- GitIgnore Generator.py
+++ GitIgnore Generator.py
@@ -1,13 +1,13 @@
 import os
 
 LF = [] #存储大小超过100 MB的文件位置（Stores paths of large files over 100 MiB）
-LNF = [] #存储文件名超过213个字符长度的文件位置（Stores paths of files with file name longer than 213 characters）
+LNF = [] #存储文件名超过171个字符长度的文件位置（Stores paths of files with file name longer than 171 characters）
 for root, dirs, files in os.walk("离线数据（Offline Data）"):
     for file in files:
         size = os.path.getsize(os.path.join(root, file))
         if size > 104857600: #100 * 1024 * 1024
             LF.append(os.path.join(root, file).replace("\\", "/"))
-        if len(file) > 213:
+        if len(file) > 171:
             LNF.append(os.path.join(root, file).replace("\\", "/"))
 
 with open(".gitignore", "w", encoding = "utf-8") as fp:
@@ -18,8 +18,8 @@ with open(".gitignore", "w", encoding = "utf-8") as fp:
             print(file)
             fp.write(file + "\n")
     if LNF:
-        print("#文件名过长（File name exceeds 213 characters）")
-        fp.write("#文件名过长（File name exceeds 213 characters）\n")
+        print("#文件名过长（File name exceeds 171 characters）")
+        fp.write("#文件名过长（File name exceeds 171 characters）\n")
         for file in LNF:
             print(file)
             fp.write(file + "\n")
